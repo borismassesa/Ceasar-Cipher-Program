ed1daee69d7f21ce8f11932a33be4d6883a85f7b
borismassesa <bmmassesa@gmail.com>
2023-02-27 18:09:29 -0800

    Changes to code

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+from art import logo
+print(logo)
 
 
 def ceasar(plain_text, shift_amount, cipher_direction):
@@ -13,15 +9,35 @@ def ceasar(plain_text, shift_amount, cipher_direction):
         shift_amount *= -1
 
     for letter in plain_text:
-        if letter == " ":
-            cipher_text += " "
-        else:
+        if letter in alphabet:
             position = alphabet.index(letter)
             new_position = (position + shift_amount) % 26
             new_text = alphabet[new_position]
             cipher_text += new_text
+        else:
+            cipher_text += letter
 
     print(f"Here is the {direction}d result: {cipher_text}")
 
 
-ceasar(plain_text=text, shift_amount=shift, cipher_direction=direction)
+flag = True
+while flag:
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z']
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n")) % 26
+
+    ceasar(plain_text=text, shift_amount=shift, cipher_direction=direction)
+
+    userInput = input("Type 'yes' if you want to continue. Otherwise type 'no'\n")
+    if userInput == "no":
+        flag = False
+        print("GoodBye!")
+
+
+
+
+
+
